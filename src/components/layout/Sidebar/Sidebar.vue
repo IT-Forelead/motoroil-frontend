2a9628f0ca70bf1a2592bfe09fb89130fe83b966
OG'ABEK <shomoyxhacker007@mail.ru>
2022-08-19 06:50:19 -0500

    Sidebar added

---
+++ src/components/layout/Sidebar/Sidebar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TopSidebarItem from './parts/TopSidebarItem.vue';
+import MiddleSidebarItem from './parts/MiddleSidebarItem.vue';
+import BottomSidebarItem from './parts/BottomSidebarItem.vue';
+
+</script>
+<template>
+ <div>
+  <aside class="col-sm-4 col-md-3 content-aside" id="column-left">
+    <TopSidebarItem/>
+    <MiddleSidebarItem/>
+    <BottomSidebarItem/>
+  </aside>
+ </div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/layout/Sidebar/parts/BottomSidebarItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <div class="module banner-left hidden-xs ">
+      <div class="banner-sidebar banners">
+        <div>
+          <a title="Banner Image" href="#">
+            <img src="/src/assets/image/catalog/banners/banner-sidebar.jpg" alt="Banner Image">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
