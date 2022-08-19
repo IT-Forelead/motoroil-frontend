e53536a95885a8f37d72cb06fa9fde7831fb2087
OG'ABEK <shomoyxhacker007@mail.ru>
2022-08-19 05:10:54 -0500

    home page completed

--- src/components/pages/Home/parts/Blogs/LatestBlogs.vue
+++ src/components/pages/Home/parts/Blogs/LatestBlogs.vue
@@ -5,68 +5,26 @@
   <div class="module so-latest-blog blog-home">
     <div class="pre_text">Our recent posts</div>
     <h3 class="modtitle"><span>Latest blogs</span></h3>
-    <div class="modcontent clearfix">
+    <div class="clearfix modcontent">
       <div class="so-blog-external buttom-type1 button-type1">
-        <div class="blog-external yt-content-slider contentslider" data-rtl="no" data-loop="no" data-autoplay="no"
-          data-autoheight="no" data-autowidth="no" data-delay="4" data-speed="0.6" data-margin="30"
-          data-items_column00="3" data-items_column0="3" data-items_column1="2" data-items_column2="2"
-          data-items_column3="1" data-items_column4="1" data-arrows="no" data-pagination="no" data-lazyload="yes"
-          data-hoverpause="yes">
-          <div class="media">
-            <div class="item head-button">
-              <div class="media-left so-block">
-                <a class="imag" href="#"><img src="/src/assets/image/catalog/blog/1.jpg" alt="image" /></a>
-              </div>
-              <div class="media-body">
-                <div class="media-content so-block">
-                  <div class="infos"><span class="media-date-added"> March 6th, 2019</span>By <span
-                      class="media-author">Wash upito</span></div>
-                  <h4 class="media-heading font-title head-item">
-                    <a href="#" title="Biten demons lector in henderit in vulp no sea takimata sanctus est"
-                      target="_self">Biten demons lector in henderit in ..</a>
-                  </h4>
-                  <div class="readmore font-title">
-                    <a href="#" target="_self"><span>Read more</span> </a>
-                  </div>
+        <div class="blog-external yt-content-slider row">
+          <div class="col-md-4" v-for="i in 3" :key="i">
+            <div class="media">
+              <div class="item head-button">
+                <div class="media-left so-block">
+                  <a class="imag" href="#"><img src="/src/assets/image/catalog/blog/1.jpg" alt="image" /></a>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="media">
-            <div class="item head-button">
-              <div class="media-left so-block">
-                <a class="imag" href="#"><img src="/src/assets/image/catalog/blog/2.jpg" alt="image" /></a>
-              </div>
-              <div class="media-body">
-                <div class="media-content so-block">
-                  <div class="infos"><span class="media-date-added"> March 6th, 2019</span>By <span
-                      class="media-author">Wash upito</span></div>
-                  <h4 class="media-heading font-title head-item">
-                    <a href="#" title="Commodo laoreet semper tincidun  sit vel illum dolore eu feugiat"
-                      target="_self">Commodo laoreet semper tincidun sit ..</a>
-                  </h4>
-                  <div class="readmore font-title">
-                    <a href="#" target="_self"><span>Read more</span> </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="media">
-            <div class="item head-button">
-              <div class="media-left so-block">
-                <a class="imag" href="#"><img src="/src/assets/image/catalog/blog/3.jpg" alt="image" /></a>
-              </div>
-              <div class="media-body">
-                <div class="media-content so-block">
-                  <div class="infos"><span class="media-date-added"> March 6th, 2019</span>By <span
-                      class="media-author">Wash upito</span></div>
-                  <h4 class="media-heading font-title head-item">
-                    <a href="#" title="Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse"
-                      target="_self">Duis autem vel eum iriure dolor ..</a>
-                  </h4>
-                  <div class="readmore font-title">
-                    <a href="#" target="_self"><span>Read more</span> </a>
+                <div class="media-body">
+                  <div class="media-content so-block">
+                    <div class="infos"><span class="media-date-added"> March 6th, 2019</span>By <span
+                        class="media-author">Wash upito</span></div>
+                    <h4 class="media-heading font-title head-item">
+                      <a href="#" title="Biten demons lector in henderit in vulp no sea takimata sanctus est"
+                        target="_self">Biten demons lector in henderit in ..</a>
+                    </h4>
+                    <div class="readmore font-title">
+                      <a href="#" target="_self"><span>Read more</span> </a>
+                    </div>
                   </div>
                 </div>
               </div>
--- src/components/pages/Home/parts/BlogsSection.vue
+++ src/components/pages/Home/parts/BlogsSection.vue
@@ -1,11 +1,9 @@
 <script setup>
-import Brands from './Blogs/Brands.vue';
 import LatestBlogs from './Blogs/LatestBlogs.vue';
 
 </script>
 <template>
   <div class="container">
-    <Brands />
     <LatestBlogs />
   </div>
 </template>
