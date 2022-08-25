6108ee7fa685304edb838ec565d380c523fa9382
Surojiddin <isurojiddin@gmail.com>
2022-08-25 17:42:54 +0500

    faq page

--- src/components/pages/Faq.vue
+++ src/components/pages/Faq.vue
@@ -1,74 +1,78 @@
+<script setup>
+import CaretRightIcon from '../../assets/icons/CaretRightIcon.vue';
+import HouseIcon from '../../assets/icons/HouseIcon.vue';
+import MinusIcon from '../../assets/icons/MinusIcon.vue';
+import PlusIcon from '../../assets/icons/PlusIcon.vue';
+</script>
+
 <template>
-	<div class="main-container container" style="margin-bottom: 3rem;">
-		<ul class="breadcrumb">
-			<li><a href="#"><i class="fa fa-home"></i></a></li>
-			<li><a href="#">Faq</a></li>
-		</ul>
-		
-		<div class="row">
-			<div id="content" class="col-sm-12">
-				<h3>Got Questions? Weve Got Answers!</h3>
-				<p>
-					Asunt in anim uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in anim id est laborum. Allamco laboris nisi ut aliquip ex ea commodo consequat. Aser velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in anim id est laborum.
-				</p>
-				<div class="row">
-					<div class="col-sm-12">
-						<ul class="yt-accordion">
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</span></h3>
-								<div class="accordion-inner">
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. </p>
-									<p>Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>Donec eros tellus scelerisque nec rhoncus eget laoreet sit amet</span></h3>
-								<div class="accordion-inner">
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. </p>
-									<p>Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>Curabitur molestie euismod erat. Proin eros odio?</span></h3>
-								<div class="accordion-inner" >
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque.</p>
-									<p> Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>In scelerisque sem at dolor. Maecenas mattis</span></h3>
-								<div class="accordion-inner">
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>Nunc feugiat mi a tellus consequat imperdiet</span></h3>
-								<div class="accordion-inner">
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>Vestibulum ante ipsum primis in faucibus orci luctus </span></h3>
-								<div class="accordion-inner" >
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-							<li class="accordion-group">
-								<h3 class="accordion-heading"><i class="fa fa-plus-square"></i><span>SmartAddons membership fee is one-time fee, or I have to pay extra?</span></h3>
-								<div class="accordion-inner" >
-									<p>Fusce eu dui. Integer vel nibh sit amet turpis vulputate aliquet. Phasellus id nisi vitae odio pretium aliquam. Pellentesque a leo. Donec consequat lectus sed felis. Quisque vestibulum massa. Nulla ornare. Nulla libero. Donec et mi eu massa ultrices scelerisque. Nullam ac nisi non eros gravida venenatis. Ut euismod, turpis sollicitudin lobortis pellentesque, libero massa dapibus dui, eu dictum justo urna et mi. Integer dictum est vitae sem. Vestibulum justo. Nulla mauris ipsum, convallis ut, vestibulum eu, tincidunt vel, nisi.</p>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
+	<div class="flex justify-center px-5 py-2 bg-white">
+		<div class="container flex flex-col justify-center">
+			<div class="p-2">
+				<ul class="flex items-center">
+					<li class="flex items-center">
+						<a href="/" class="text-gray-700">
+							<HouseIcon class="w-4 h-4" />
+						</a>
+						<CaretRightIcon class="mx-3 text-gray-500" />
+					</li>
+					<li class="text-gray-700">FAQ</li>
+				</ul>
+			</div>
+			<div class="flex flex-col justify-center">
+				<div class="text-2xl font-semibold text-center text-gray-800 lg:text-4xl">Have any Questions?</div>
+				<ul class="flex flex-col justify-center mx-48 divide-y">
+					<li class="py-5">
+						<button class="flex items-center focus:outline-none">
+							<MinusIcon class="w-5 h-5 text-red-500"/>
+							<span class="mx-4 text-xl text-gray-700">
+								How i can play for my appoinment ?
+							</span>
+						</button>
+						<div class="p-2 my-5 ml-10 border-l-2 border-red-500">
+							<p class="px-2 text-gray-500">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam quidem.
+							</p>
+						</div>
+					</li>
+					<li class="py-5">
+						<button class="flex items-center focus:outline-none">
+							<PlusIcon class="w-5 h-5 text-red-500"/>
+							<span class="mx-4 text-xl text-gray-700">
+								What can i expect at my first consultation ?
+							</span>
+						</button>
+					</li>
+					<li class="py-5">
+						<button class="flex items-center focus:outline-none">
+							<PlusIcon class="w-5 h-5 text-red-500"/>
+							<span class="mx-4 text-xl text-gray-700">
+								What are your opening house ?
+							</span>
+						</button>
+					</li>
+					<li class="py-5">
+						<button class="flex items-center focus:outline-none">
+							<PlusIcon class="w-5 h-5 text-red-500"/>
+							<span class="mx-4 text-xl text-gray-700">
+								Do i need a referral ?
+							</span>
+						</button>
+					</li>
+					<li class="py-5">
+						<button class="flex items-center focus:outline-none">
+							<PlusIcon class="w-5 h-5 text-red-500"/>
+							<span class="mx-4 text-xl text-gray-700">
+								Is the cost of the appoinment covered by private health insurance ?
+							</span>
+						</button>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script setup>
-</script>
 
 <style scoped>
 </style>
