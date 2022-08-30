62824e498c499ac32d25d2cf895fa738f3e84eb0
Surojiddin <isurojiddin@gmail.com>
2022-08-30 17:20:55 +0500

    store api

--- src/components/pages/Admin/Store.vue
+++ src/components/pages/Admin/Store.vue
@@ -5,8 +5,23 @@ import StackIcon from '../../../assets/icons/StackIcon.vue';
 import CalendarCheckIcon from '../../../assets/icons/CalendarCheckIcon.vue';
 import CalendarXIcon from '../../../assets/icons/CalendarXIcon.vue';
 import PencilDuotoneIcon from '../../../assets/icons/PencilDuotoneIcon.vue';
+import { useProductStore } from '../../../stores/product.js'
 import { ref } from '@vue/reactivity';
 import { onClickOutside } from '@vueuse/core'
+import { onMounted } from '@vue/runtime-core';
+
+const API_URL = import.meta.env.VITE_MY_ENV_VARIABLE
+const store = useProductStore()
+
+onMounted(() => {
+  store.getAllProducts()
+})
+
+const showProduct = (id) => {
+  store.getSingleProduct(id)
+  sessionStorage.removeItem('sp_id')
+  sessionStorage.setItem('sp_id', id)
+}
 
 const isOpenSortDropDown = ref(false)
 const sortDropDown = ref(null)
@@ -69,168 +84,52 @@ onClickOutside(sortDropDown, () => isOpenSortDropDown.value = false)
             </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
-            <tr class="align-middle">
+            <tr v-for="(product, idx) in store.products" :key="idx" class="align-middle">
               <td class="p-3 text-sm text-gray-700">
                 <div class="flex items-center space-x-2">
+                  <router-link to="/product" @click="showProduct(product?.product?.id)">
+                    <img class="w-full h-16" :src="API_URL + '/image/' + (product?.product?.imageUrl[0] ? product?.product?.imageUrl[0] : '')" alt="product image">
+                  </router-link>
                   <div>
-                    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="w-full h-12" alt="#">
-                  </div>
-                  <div>
-                    <div class="font-medium text-gray-700 text-md">Prista Extra W10-50</div>
+                    <router-link to="/product" class="font-medium text-gray-700 text-md" @click="showProduct(product?.product?.id)"
+                      :title="product?.productGroup?.productGroup?.name + ' ' + product?.productGroup?.viscosityGrade?.name"
+                      target="_self">
+                      {{ product?.productGroup?.productGroup?.name + ' ' + product?.productGroup?.viscosityGrade?.name }}
+                    </router-link>
                     <div class="flex items-center text-sm text-gray-500">
                       <StackIcon class="mr-1"/>
-                      Automotivation
+                      {{ product?.productGroup?.category?.name }}
                     </div>
                   </div>
                 </div>
               </td>
-              <td class="p-3 font-medium text-gray-700">
-                $123.0
-              </td>
+              <td class="p-3 font-medium text-gray-700">€ {{ product?.product?.price }}</td>
               <td class="p-3">
-                <div class="flex items-center">
+                <div class="flex items-center" v-if="product?.discount">
                   <div class="mr-1 text-sm text-gray-500"><CalendarCheckIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
+                  <div class="font-medium text-gray-700 text-md">{{ product?.discount?.startedAt }}</div>
                 </div>
-                <div class="flex items-center">
+                <div class="flex items-center" v-if="product?.discount">
                   <div class="mr-1 text-sm text-gray-500"><CalendarXIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
+                  <div class="font-medium text-gray-700 text-md">{{ product?.discount?.expiredAt }}</div>
                 </div>
               </td>
               <td class="p-3">
                 <div class="flex items-center">
                   <div class="mr-1 text-sm text-gray-500">Quantity:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
+                  <div class="font-medium text-gray-700 text-md">{{ product?.product?.quantity }}</div>
                 </div>
                 <div class="flex items-center">
                   <div class="mr-1 text-sm text-gray-500">Sold product:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
+                  <div class="font-medium text-gray-700 text-md">{{ product?.product?.orders }}</div>
                 </div>
                 <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Viewsd:</div>
-                  <div class="font-medium text-gray-700 text-md">12342</div>
+                  <div class="mr-1 text-sm text-gray-500">Viewed:</div>
+                  <div class="font-medium text-gray-700 text-md">{{ product?.product?.views }}</div>
                 </div>
               </td>
               <td class="p-3">
-                 <label for="holiday-toggle" class="relative inline-flex items-center mr-3 cursor-pointer">
-                  <input type="checkbox" id="holiday-toggle" class="sr-only peer" />
-                  <div class="h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700"></div>
-                </label>
-              </td>
-              <td class="p-3">
-                <div class="flex items-start space-x-2">
-                  <button class="flex items-center justify-center p-2 text-white bg-red-500 rounded hover:bg-red-700">
-                    <PencilDuotoneIcon class="w-4 h-4" />
-                  </button>
-                  <button class="flex items-center justify-center p-2 text-white bg-red-500 rounded hover:bg-red-700">
-                    <TrashIcon />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr class="align-middle">
-              <td class="p-3 text-sm text-gray-700">
-                <div class="flex items-center space-x-2">
-                  <div>
-                    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="w-full h-12" alt="#">
-                  </div>
-                  <div>
-                    <div class="font-medium text-gray-700 text-md">Prista Extra W10-50</div>
-                    <div class="flex items-center text-sm text-gray-500">
-                      <StackIcon class="mr-1"/>
-                      Automotivation
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td class="p-3 font-medium text-gray-700">
-                $123.0
-              </td>
-              <td class="p-3">
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500"><CalendarCheckIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500"><CalendarXIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
-                </div>
-              </td>
-              <td class="p-3">
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Quantity:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Sold product:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Viewsd:</div>
-                  <div class="font-medium text-gray-700 text-md">12342</div>
-                </div>
-              </td>
-              <td class="p-3">
-                 <label for="holiday-toggle" class="relative inline-flex items-center mr-3 cursor-pointer">
-                  <input type="checkbox" id="holiday-toggle" class="sr-only peer" />
-                  <div class="h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700"></div>
-                </label>
-              </td>
-              <td class="p-3">
-                <div class="flex items-start space-x-2">
-                  <button class="flex items-center justify-center p-2 text-white bg-red-500 rounded hover:bg-red-700">
-                    <PencilDuotoneIcon class="w-4 h-4" />
-                  </button>
-                  <button class="flex items-center justify-center p-2 text-white bg-red-500 rounded hover:bg-red-700">
-                    <TrashIcon />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr class="align-middle">
-              <td class="p-3 text-sm text-gray-700">
-                <div class="flex items-center space-x-2">
-                  <div>
-                    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" class="w-full h-12" alt="#">
-                  </div>
-                  <div>
-                    <div class="font-medium text-gray-700 text-md">Prista Extra W10-50</div>
-                    <div class="flex items-center text-sm text-gray-500">
-                      <StackIcon class="mr-1"/>
-                      Automotivation
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td class="p-3 font-medium text-gray-700">
-                $123.0
-              </td>
-              <td class="p-3">
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500"><CalendarCheckIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500"><CalendarXIcon class="w-4 h-4 mr-1"/></div>
-                  <div class="font-medium text-gray-700 text-md">28.08.2022 17:30</div>
-                </div>
-              </td>
-              <td class="p-3">
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Quantity:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Sold product:</div>
-                  <div class="font-medium text-gray-700 text-md">123</div>
-                </div>
-                <div class="flex items-center">
-                  <div class="mr-1 text-sm text-gray-500">Viewsd:</div>
-                  <div class="font-medium text-gray-700 text-md">12342</div>
-                </div>
-              </td>
-              <td class="p-3">
-                 <label for="holiday-toggle" class="relative inline-flex items-center mr-3 cursor-pointer">
+                <label for="holiday-toggle" class="relative inline-flex items-center mr-3 cursor-pointer" v-if="product?.discount">
                   <input type="checkbox" id="holiday-toggle" class="sr-only peer" />
                   <div class="h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700"></div>
                 </label>
