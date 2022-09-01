69a490e90473d557a6b2dff74c013857496d17cf
Bekturdi <48916520+Bekturdi@users.noreply.github.com>
2022-09-01 16:05:20 +0500

    products page added translate

--- src/components/pages/Products/Product.vue
+++ src/components/pages/Products/Product.vue
@@ -106,7 +106,7 @@ const addComment = () => {
             <CaretRightIcon class="mx-3 text-gray-500" />
           </li>
           <li class="flex items-center">
-            <a href="/products" class="text-gray-700">Products</a>
+            <a href="/products" class="text-gray-700">{{$t('products')}}</a>
             <CaretRightIcon class="mx-3 text-gray-500" />
           </li>
           <li class="text-gray-700">{{ store.singleProduct?.productGroup?.name + ' ' +
@@ -164,7 +164,7 @@ const addComment = () => {
                 </div>
                 <div v-if="store.singleProduct?.product?.quantity !== 0"
                   class="flex items-center justify-center w-24 px-2 py-1 text-xs text-green-500 bg-green-100 rounded-full">
-                  <CheckCircleFillIcon class="mr-1" /> in stock
+                  <CheckCircleFillIcon class="mr-1" /> {{$t('inStock')}}
                 </div>
               </div>
               <div class="py-3">
@@ -174,12 +174,12 @@ const addComment = () => {
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.specTypeValue?.value }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">Viscosity Grade:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('viscosityGrade')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.viscosityGrade?.name
                     }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">Brand:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('brand')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.brand?.name }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
@@ -189,21 +189,21 @@ const addComment = () => {
                     }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">Spectification:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('spectification')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{
                         store.oemsAndSpecsByProductId?.specifications?.map(o => o.name).join(', ')
                     }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">View:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('view')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.product?.views }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">Likes:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('likes')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.product?.likes }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
-                    <span class="font-medium text-gray-700 text-md">Orders:</span>
+                    <span class="font-medium text-gray-700 text-md">{{$t('orders')}}:</span>
                     <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.product?.orders }}</span>
                   </li>
                 </ul>
@@ -246,7 +246,7 @@ const addComment = () => {
                     </button>
                     <button class="flex items-center justify-center w-full p-3 text-white bg-red-500 hover:bg-red-700">
                       <ShoppingCartFillIcon class="mr-2" />
-                      Add to cart
+                      {{$t('addToCart')}}
                     </button>
                   </div>
                   <div v-for="(product, idx) in store.productsByGroupId.filter(p => p?.product?.id !== store.singleProduct?.product?.id)" :key="idx" class="flex items-center px-3 py-1 space-x-3 border border-gray-300">
@@ -274,19 +274,19 @@ const addComment = () => {
               </div>
               <div class="py-3 space-y-2">
                 <div class="flex items-center space-x-2">
-                  <span class="font-normal text-gray-700 text-md">Category:</span>
-                  <span class="font-medium text-gray-700 text-md">Automotive</span>
+                  <span class="font-normal text-gray-700 text-md">{{$t('category')}}:</span>
+                  <span class="font-medium text-gray-700 text-md">{{$t('automotive')}}</span>
                 </div>
                 <div class="flex items-center space-x-3">
                   <a v-if="store.singleProduct?.productGroup?.pdfUrl" :href="`${API_URL}/image/${store.singleProduct?.productGroup?.pdfUrl}`" download
                     class="flex items-center justify-center px-5 py-2 text-blue-500 bg-white border border-blue-500 rounded hover:bg-gray-100">
                     <PdfFileIcon class="mr-1" />
-                    Download PDF
+                    {{$t('downloadPDF')}}
                   </a>
                   <button v-if="authStore.user?.role === 'admin'"
                     class="flex items-center justify-center px-5 py-2 text-red-500 bg-white border border-red-500 rounded hover:bg-gray-100">
                     <PdfFileIcon class="mr-1" />
-                    Upload PDF
+                    {{$t('uploadPDF')}}
                   </button>
                 </div>
               </div>
@@ -307,8 +307,7 @@ const addComment = () => {
               <div id="review" class="space-y-2" v-else>
                 <div v-if="authStore.user?.role === 'user'" class="pb-3 mb-3 border-b">
                   <div class="max-w-2xl space-y-1">
-                    <label for="message" class="block mb-2 font-medium text-gray-900 text-md dark:text-gray-400">Write a
-                      review</label>
+                    <label for="message" class="block mb-2 font-medium text-gray-900 text-md dark:text-gray-400">{{$t('writeReview')}}</label>
                     <textarea id="message" rows="4" v-model="comment.message"
                       class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                       placeholder="Your review..."></textarea>
@@ -322,7 +321,7 @@ const addComment = () => {
                     </div>
                     <button @click="addComment()"
                       class="block items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-red-500 rounded focus:ring-4 focus:ring-red-200 hover:bg-red-700">
-                      Comment
+                      {{$t('comment')}}
                     </button>
                   </div>
                 </div>
@@ -352,12 +351,12 @@ const addComment = () => {
   <div class="relative w-full h-full max-w-5xl p-4 -translate-x-1/2 -translate-y-1/2 md:h-auto top-1/2 left-1/2">
     <div class="relative bg-white rounded shadow dark:bg-gray-700">
       <div class="flex items-start justify-between px-6 py-3 border-b rounded-t dark:border-gray-600">
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Product video</h3>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{$t('productVideo')}}</h3>
         <button type="button" @click="modalStore.closeVideoModal()"
           class="inline-flex items-center p-1 ml-auto text-sm text-gray-400 bg-transparent rounded hover:bg-gray-200 hover:text-gray-900"
           data-modal-toggle="defaultModal">
           <CloseIcon />
-          <span class="sr-only">Close modal</span>
+          <span class="sr-only">{{$t('closeModal')}}</span>
         </button>
       </div>
       <div >
