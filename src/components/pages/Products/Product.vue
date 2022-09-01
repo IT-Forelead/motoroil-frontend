78e7a4df100769374d4493212e562bc7c9016bee
OG'ABEK <shomoyxhacker007@mail.ru>
2022-09-01 03:19:35 -0500

    delete product api connected

--- src/components/pages/Products/Product.vue
+++ src/components/pages/Products/Product.vue
@@ -23,10 +23,12 @@ import Rating from '../../Rating.vue';
 import { uuid } from 'vue-uuid';
 import $ from 'jquery';
 import HeartFillIcon from '../../../assets/icons/HeartFillIcon.vue';
-import router from '../../../router';
 import YoutubeIcon from '../../../assets/icons/YoutubeIcon.vue';
 import CloseIcon from '../../../assets/icons/CloseIcon.vue';
+import { useRouter } from 'vue-router';
+
 const API_URL = import.meta.env.VITE_MY_ENV_VARIABLE
+const router = useRouter()
 const store = useProductStore()
 const authStore = useAuthStore()
 const modalStore = useModalStore()
@@ -93,6 +95,12 @@ const addComment = () => {
   comment.message = ''
   comment.rating = 5
 }
+
+const deleteCurrentProduct = (id) => {
+  store.deleteProduct(id);
+  sessionStorage.removeItem('sp_id')
+  router.push('/products')
+}
 </script>
 <template>
   <div class="flex justify-center px-5 py-2 bg-white">
@@ -121,13 +129,15 @@ const addComment = () => {
             <div>
               <div class="relative h-[300px] overflow-hidden">
                 <div v-if="store.singleProduct?.discount" class="absolute top-5 left-5">
-                  <div class="relative z-10 flex items-center justify-center w-10 h-10 font-semibold bg-yellow-300 discount">
+                  <div
+                    class="relative z-10 flex items-center justify-center w-10 h-10 font-semibold bg-yellow-300 discount">
                     <span class="text-xl">-{{ store.singleProduct?.discount?.discountPercent }}</span>
                     <span class="text-xs">%</span>
                   </div>
                 </div>
-                <div v-if="store.singleProduct?.productGroup?.videoUrl" @click="modalStore.openVideoModal()" class="absolute z-10 bottom-5 right-5">
-                  <YoutubeIcon class="w-10 h-10 text-gray-700 cursor-pointer hover:text-red-500"/>
+                <div v-if="store.singleProduct?.productGroup?.videoUrl" @click="modalStore.openVideoModal()"
+                  class="absolute z-10 bottom-5 right-5">
+                  <YoutubeIcon class="w-10 h-10 text-gray-700 cursor-pointer hover:text-red-500" />
                 </div>
                 <img v-if="store.singleProduct?.product?.imageUrl[0]"
                   class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
@@ -156,10 +166,12 @@ const addComment = () => {
                   <div v-if="!store.singleProduct?.discount" class="mb-2 mr-3 text-lg font-semibold text-red-500">€
                     {{ store.singleProduct?.product?.price }}</div>
                   <div v-else class="flex items-center justify-center">
-                    <div class="mb-2 mr-3 text-lg font-semibold text-red-500">€ {{ (store.singleProduct?.product?.price -
+                    <div class="mb-2 mr-3 text-lg font-semibold text-red-500">€ {{ (store.singleProduct?.product?.price
+                        -
                         store.singleProduct?.product?.price * (store.singleProduct?.discount?.discountPercent / 100))
                     }}</div>
-                    <div class="mb-2 text-gray-500 line-through text-md">€ {{ store.singleProduct?.product?.price }}</div>
+                    <div class="mb-2 text-gray-500 line-through text-md">€ {{ store.singleProduct?.product?.price }}
+                    </div>
                   </div>
                 </div>
                 <div v-if="store.singleProduct?.product?.quantity !== 0"
@@ -171,7 +183,8 @@ const addComment = () => {
                 <ul class="flex flex-col p-2 space-y-1 border-l-4 border-red-500">
                   <li class="flex items-center space-x-2">
                     <span class="font-medium text-gray-700 text-md">{{ store.singleProduct?.specType?.name }}:</span>
-                    <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.specTypeValue?.value }}</span>
+                    <span class="font-normal text-gray-700 text-md">{{ store.singleProduct?.specTypeValue?.value
+                    }}</span>
                   </li>
                   <li class="flex items-center space-x-2">
                     <span class="font-medium text-gray-700 text-md">Viscosity Grade:</span>
@@ -208,6 +221,19 @@ const addComment = () => {
                   </li>
                 </ul>
               </div>
+              <div class="py-3">
+                <ul class="flex flex-col p-2 space-y-1 border-l-4 border-red-500">
+                  <li class="flex items-center space-x-2"
+                    @click="deleteCurrentProduct(store.singleProduct?.product?.id)">
+                    <span class="font-medium text-gray-500 cursor-pointer hover:text-red-600 text-md">Delete
+                      product</span>
+                  </li>
+                  <li class="flex items-center space-x-2">
+                    <span class="font-medium text-gray-500 cursor-pointer hover:text-blue-600 text-md">Edit
+                      product</span>
+                  </li>
+                </ul>
+              </div>
               <div v-if="authStore.user?.role !== 'admin'" class="py-3">
                 <div class="space-y-2 w-72">
                   <div class="flex items-center px-3 py-1.5 space-x-3 border border-gray-300">
@@ -215,16 +241,21 @@ const addComment = () => {
                       class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500" checked>
                     <OilerIcon class="w-5 h-5" />
                     <label for="lll" class="block ml-3 text-sm font-medium text-gray-700">
-                      <div class="font-medium text-md">{{ store.singleProduct?.specType?.name + ': ' + store.singleProduct?.specTypeValue?.value }}</div>
+                      <div class="font-medium text-md">{{ store.singleProduct?.specType?.name + ': ' +
+                          store.singleProduct?.specTypeValue?.value
+                      }}</div>
                       <div class="text-sm font-normal">
                         <div v-if="!store.singleProduct?.discount" class="text-red-500">
                           € {{ store.singleProduct?.product?.price }}
                         </div>
                         <div v-else class="flex items-center justify-center">
                           <div class="mr-2 text-red-500">
-                            € {{ (store.singleProduct?.product?.price - store.singleProduct?.product?.price * (store.singleProduct?.discount?.discountPercent / 100)) }}
+                            € {{ (store.singleProduct?.product?.price - store.singleProduct?.product?.price *
+                                (store.singleProduct?.discount?.discountPercent / 100))
+                            }}
                           </div>
-                          <div class="text-gray-500 line-through text-md">€ {{ store.singleProduct?.product?.price }}</div>
+                          <div class="text-gray-500 line-through text-md">€ {{ store.singleProduct?.product?.price }}
+                          </div>
                         </div>
                       </div>
                     </label>
@@ -241,7 +272,8 @@ const addComment = () => {
                     </button>
                   </div>
                   <div class="flex items-center space-x-2">
-                    <button @click="likeFunction()" class="flex items-center justify-center p-3.5 text-white bg-red-500 hover:bg-red-700">
+                    <button @click="likeFunction()"
+                      class="flex items-center justify-center p-3.5 text-white bg-red-500 hover:bg-red-700">
                       <HeartFillIcon class="w-5 h-5" />
                     </button>
                     <button class="flex items-center justify-center w-full p-3 text-white bg-red-500 hover:bg-red-700">
@@ -249,11 +281,15 @@ const addComment = () => {
                       Add to cart
                     </button>
                   </div>
-                  <div v-for="(product, idx) in store.productsByGroupId.filter(p => p?.product?.id !== store.singleProduct?.product?.id)" :key="idx" class="flex items-center px-3 py-1 space-x-3 border border-gray-300">
-                    <input @click="showProduct(product?.product?.id)" :id="`cp-${product?.product?.id}`" name="push-notifications" type="radio"
+                  <div
+                    v-for="(product, idx) in store.productsByGroupId.filter(p => p?.product?.id !== store.singleProduct?.product?.id)"
+                    :key="idx" class="flex items-center px-3 py-1 space-x-3 border border-gray-300">
+                    <input @click="showProduct(product?.product?.id)" :id="`cp-${product?.product?.id}`"
+                      name="push-notifications" type="radio"
                       class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500">
                     <OilerIcon class="w-5 h-5" />
-                    <label @click="showProduct(product?.product?.id)" :for="`cp-${product?.product?.id}`" class="block ml-3 text-sm font-medium text-gray-700">
+                    <label @click="showProduct(product?.product?.id)" :for="`cp-${product?.product?.id}`"
+                      class="block ml-3 text-sm font-medium text-gray-700">
                       <div class="font-medium text-md">
                         {{ product?.specType?.name + ': ' + product?.specTypeValue?.value }}
                       </div>
@@ -263,7 +299,9 @@ const addComment = () => {
                         </div>
                         <div v-else class="flex items-center justify-center">
                           <div class="mr-2 text-red-500">
-                            € {{ (product?.product?.price - product?.product?.price * (product?.discount?.discountPercent / 100)) }}
+                            € {{ (product?.product?.price - product?.product?.price *
+                                (product?.discount?.discountPercent / 100))
+                            }}
                           </div>
                           <div class="text-gray-500 line-through text-md">€ {{ product?.product?.price }}</div>
                         </div>
@@ -278,7 +316,8 @@ const addComment = () => {
                   <span class="font-medium text-gray-700 text-md">Automotive</span>
                 </div>
                 <div class="flex items-center space-x-3">
-                  <a v-if="store.singleProduct?.productGroup?.pdfUrl" :href="`${API_URL}/image/${store.singleProduct?.productGroup?.pdfUrl}`" download
+                  <a v-if="store.singleProduct?.productGroup?.pdfUrl"
+                    :href="`${API_URL}/image/${store.singleProduct?.productGroup?.pdfUrl}`" download
                     class="flex items-center justify-center px-5 py-2 text-blue-500 bg-white border border-blue-500 rounded hover:bg-gray-100">
                     <PdfFileIcon class="mr-1" />
                     Download PDF
@@ -315,7 +354,8 @@ const addComment = () => {
                     <div class="flex items-center space-x-2">
                       <span class="text-sm text-gray-500">Rating:</span>
                       <ul class="flex items-center py-2">
-                        <li v-for="i in 5" :key="i" @click="comment.rating = i; currentRating = comment.rating" class="cursor-pointer">
+                        <li v-for="i in 5" :key="i" @click="comment.rating = i; currentRating = comment.rating"
+                          class="cursor-pointer">
                           <StarFillIcon :class="i <= currentRating ? 'text-yellow-300' : 'text-gray-300'" />
                         </li>
                       </ul>
@@ -326,7 +366,8 @@ const addComment = () => {
                     </button>
                   </div>
                 </div>
-                <div v-for="(comment, idx) in store.comments" :key="idx" class="flex items-start px-4 py-3 border rounded">
+                <div v-for="(comment, idx) in store.comments" :key="idx"
+                  class="flex items-start px-4 py-3 border rounded">
                   <div class="flex items-center justify-center w-12 h-12 mr-4 bg-red-500 rounded-full shadow">
                     <UserIcon class="w-8 h-8 text-white" />
                   </div>
@@ -347,35 +388,41 @@ const addComment = () => {
     </div>
   </div>
 
-<!-- Video Modal -->
-<div :class="{ 'hidden': !modalStore.isOpenVideoModal }" class="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full backdrop-blur bg-gray-900/50">
-  <div class="relative w-full h-full max-w-5xl p-4 -translate-x-1/2 -translate-y-1/2 md:h-auto top-1/2 left-1/2">
-    <div class="relative bg-white rounded shadow dark:bg-gray-700">
-      <div class="flex items-start justify-between px-6 py-3 border-b rounded-t dark:border-gray-600">
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Product video</h3>
-        <button type="button" @click="modalStore.closeVideoModal()"
-          class="inline-flex items-center p-1 ml-auto text-sm text-gray-400 bg-transparent rounded hover:bg-gray-200 hover:text-gray-900"
-          data-modal-toggle="defaultModal">
-          <CloseIcon />
-          <span class="sr-only">Close modal</span>
-        </button>
-      </div>
-      <div >
-        <iframe class="w-full aspect-video" :src="store.singleProduct?.productGroup?.videoUrl" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <!-- Video Modal -->
+  <div :class="{ 'hidden': !modalStore.isOpenVideoModal }"
+    class="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full backdrop-blur bg-gray-900/50">
+    <div class="relative w-full h-full max-w-5xl p-4 -translate-x-1/2 -translate-y-1/2 md:h-auto top-1/2 left-1/2">
+      <div class="relative bg-white rounded shadow dark:bg-gray-700">
+        <div class="flex items-start justify-between px-6 py-3 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Product video</h3>
+          <button type="button" @click="modalStore.closeVideoModal()"
+            class="inline-flex items-center p-1 ml-auto text-sm text-gray-400 bg-transparent rounded hover:bg-gray-200 hover:text-gray-900"
+            data-modal-toggle="defaultModal">
+            <CloseIcon />
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <div>
+          <iframe class="w-full aspect-video" :src="store.singleProduct?.productGroup?.videoUrl" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
 .discount:before {
   transform: rotate(30deg);
 }
+
 .discount:after {
   transform: rotate(60deg);
 }
-.discount:before, .discount:after {
+
+.discount:before,
+.discount:after {
   content: "";
   position: absolute;
   z-index: -1;
