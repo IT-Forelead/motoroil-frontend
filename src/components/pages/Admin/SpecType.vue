adf32d2b8243fde2fd42adc7043770fa57796a8c
Surojiddin <isurojiddin@gmail.com>
2022-09-01 16:25:09 +0500

    spec type page api

--- src/components/pages/Admin/SpecType.vue
+++ src/components/pages/Admin/SpecType.vue
@@ -1,8 +1,25 @@
 
 <script setup>
-import HouseIcon from '../../../assets/icons/HouseIcon.vue'
-import CaretRightIcon from '../../../assets/icons/CaretRightIcon.vue';
 import TrashIcon from '../../../assets/icons/TrashIcon.vue';
+import { useProductStore } from '../../../stores/product.js'
+import { onMounted, reactive } from '@vue/runtime-core';
+import { uuid } from 'vue-uuid';
+
+const store = useProductStore()
+
+const specType = reactive({
+  id: uuid.v4(),
+  name: ''
+})
+
+const addSpecType = () => {
+    store.createSpecType(specType)
+    specType.name = ''
+}
+
+onMounted(() => {
+  store.getAllSpecTypes()
+})
 
 </script>
 <template>
@@ -12,24 +29,18 @@ import TrashIcon from '../../../assets/icons/TrashIcon.vue';
                 <div class="col-span-2 ml-3">
                     <div class="p-3 text-2xl font-semibold text-gray-700">Specification Types</div>
                     <ul class="flex flex-col divide-y">
-                        <li class="flex items-center justify-between px-3 py-2">
-                            asasdssdsd
-                            <TrashIcon class="w-5 h-5 text-red-500"/>
-                        </li>
-                        <li class="flex items-center justify-between px-3 py-2">
-                            asasdssdsd
-                            <TrashIcon class="w-5 h-5 text-red-500"/>
-                        </li>
-                        <li class="flex items-center justify-between px-3 py-2">
-                            asasdssdsd
-                            <TrashIcon class="w-5 h-5 text-red-500"/>
+                        <li v-for="(specType, idx) in store.specTypes" :key="idx" class="flex items-center justify-between px-3 py-2">
+                            {{ specType?.name }}
+                            <div @click="store.deleteSpecType(specType?.id)">
+                                <TrashIcon class="w-5 h-5 text-red-500"/>
+                            </div>
                         </li>
                     </ul>
                 </div>
                 <div class="p-5 space-y-4 bg-white rounded shadow">
                     <div class="p-3 font-medium text-gray-700 border-b text-md">Add Specification Types</div>
-                    <input type="text" class="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Enter specification types...">
-                    <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">Save</button>
+                    <input v-model="specType.name" type="text" class="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Enter specification types...">
+                    <button @click="addSpecType()" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">Save</button>
                 </div>
             </div>
         </div>
