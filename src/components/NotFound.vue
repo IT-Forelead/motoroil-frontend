80f12888ea9b049342f9b7df38e9341d406022cb
Safabayev <75213623+Safabayev@users.noreply.github.com>
2022-09-01 12:45:07 +0500

    Translate

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -5,11 +5,11 @@
         <div class="flex flex-col items-center justify-center md:py-24 lg:py-32">
           <h1 class="font-bold text-blue-600 text-9xl">404</h1>
           <p class="mb-2 text-2xl font-bold text-center text-gray-800 dark:text-gray-300 md:text-3xl">
-            <span class="text-red-500">Oops!</span> Page Not Found!
+            <span class="text-red-500">{{$t('Oops')}}</span>{{$t('404')}}
           </p>
           <router-link to="/"
             class="px-6 py-2 mt-6 text-sm font-semibold text-blue-800 transition bg-blue-100 rounded hover:text-white hover:bg-indigo-500">
-            Home page</router-link>
+            {{$t('homePage')}}</router-link>
         </div>
       </div>
     </div>
