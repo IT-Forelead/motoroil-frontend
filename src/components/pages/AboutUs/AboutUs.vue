42062a2c35ed3889c070e02e51a66b00b2b14de7
Surojiddin <isurojiddin@gmail.com>
2022-09-06 18:24:11 +0500

    about us page not finished

--- src/components/pages/AboutUs/AboutUs.vue
+++ src/components/pages/AboutUs/AboutUs.vue
@@ -1,88 +1,43 @@
-<template>
-	<div class="container main-container" style="margin-bottom: 3rem;">
-		<ul class="breadcrumb">
-			<li><a href="#"><i class="fa fa-home"></i></a></li>
-			<li><a href="#">{{$t('aboutUs')}}</a></li>
-		</ul>
-		
-		<div class="row">			
-      <div id="content" class="col-sm-12 item-article">
-        <div class="row box-1-about">
-          <div class="col-md-9 welcome-about-us">
-            <div class="title-about-us">
-              <h2>{{$t('welcomeToShop')}}</h2>
-            </div>
-            <div class="content-about-us">
-              <div class="image-about-us">
-                <!-- <img src="/src/assets/image/catalog/about/about-us.jpg" alt="Image Client"> -->
-              </div>
-              <div class="des-about-us">
-                Nulla auctor mauris ut dui luctus semper. In hac habitasse platea dictumst. Duis pellentesque ligula a risus suscipit dignissim. Nunc non nisl lacus. Integer pharetra lacinia dapibus. Donec eu dolor dui, vel posuere mauris.
-                <br>
-                <br>Pellentesque semper congue sodales. In consequat, metus eget con sequat ornare, augue dolor blandit purus, vitae lacinia nisi tellus in erat. Nulla ac justo eget massa aliquet sodales. Maecenas mattis male suada sem, in fringilla massa dapibus quis. Suspendisse aliquam leo id neque auctor molestie. Etiam at nulla tellus.
-                <br>
-                <br>Nulla auctor mauris ut dui luctus semper. In hac habitasse platea dictumst. Duis pellentesque ligula a risus suscipit dignissim.
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3 why-choose-us">
-            <div class="title-about-us">
-              <h2>{{$t('whyChooseUs')}}</h2>
-            </div>
-            <div class="content-why">
-              <ul class="why-list">
-                <li><a title="Shipping &amp; Returns" href="#">{{$t('shippingAndReturns')}}</a></li>
-                <li><a title="Secure Shopping" href="#">{{$t('secureShopping')}}</a></li>
-                <li><a title="International Shipping" href="#">{{$t('internationalShipping')}}</a></li>
-                <li><a title="Affiliates" href="#">{{$t('affiliates')}}</a></li>
-                <li><a title="Group Sales" href="#">{{$t('groupSales')}}</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-12 our-member">
-            <div class="title-about-us">
-              <h2>{{$t('ourMember')}}</h2>
-            </div>
-            <div class="short-des">
-              Consectetur adipiscing elit. Donec pellentesque venenatis elit, quis aliquet mauris malesuada vel. Donec vitae libero dolor, eget dapibus justo.
-              <br>Aenean facilisis aliquet feugiat. Suspendisse lacinia congue est ac semper. Nulla ut elit magna, vitae volutpat magna.
-            </div>
-            <div class="overflow-owl-slider1">
-              <div class="wrapper-owl-slider1">
-                <div class="row slider-ourmember">
-                  <MemberItem />
-                  <MemberItem />
-                  <MemberItem />
-                  <MemberItem />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-12 happy-about-us">
-            <div id="slider-happy-about-us" class="happy-ab">
-              <div class="title-happy-about">
-                <h2>{{$t('happyCustomerSays')}}</h2>
-              </div>
-              <div class="yt-content-slider sm_imageslider slider-happy-client" data-rtl="yes" data-autoplay="no" data-autoheight="no" data-delay="4" data-speed="0.6" data-margin="0" data-items_column00="1" data-items_column0="1" data-items_column1="1" data-items_column2="1"  data-items_column3="1" data-items_column4="1" data-arrows="yes" data-pagination="no" data-lazyload="yes" data-loop="no" data-hoverpause="yes">
-                <div class="item">
-                  <div class="ct-why">
-                    <div class="client-say">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue, justo non cursus adipiscing, dui nibh scelerisque justo, quis pretium turpis neque eget nulla. Curabitur dictum consectetur metus nec dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue, justo non cursus adipiscing, dui nibh scelerisque justo non cursus adipiscing, dui nibh scelerisque justo, quis pretium turpis.</div>
-                    <p class="client-info-about"><span class="name">- Mama Duo - </span>Social Media Strategist</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-		</div>
-	</div>
-</template>
-
 <script setup>
 import MemberItem from './parts/MemberItem.vue';
+import HouseIcon from '../../../assets/icons/HouseIcon.vue';
+import CaretRightIcon from '../../../assets/icons/CaretRightIcon.vue';
+import Sidebar from '../../layout/Sidebar/Sidebar.vue';
 
 </script>
+
+<template>
+<div class="flex justify-center px-5 py-2 bg-white">
+  <div class="container flex flex-col justify-center">
+    <div class="p-2">
+      <ul class="flex items-center">
+        <li class="flex items-center">
+          <a href="/" class="text-gray-700">
+              <HouseIcon class="w-4 h-4" />
+          </a>
+          <CaretRightIcon class="mx-3 text-gray-500" />
+        </li>
+        <li class="flex items-center">
+          <a href="/about-us" class="text-red-600">About Us</a>
+        </li>
+      </ul>
+    </div>
+    <div class="grid grid-cols-4 gap-3">
+      <Sidebar />
+      <div class="col-span-3 ml-3">
+        <div class="p-3 text-2xl font-semibold text-gray-700">Welcome To Shop</div>
+        <div>
+          <img class="float-left" src="path/to/image.jpg">
+          <p>
+            Maybe we can live without libraries, people like you and me.
+            Lorem ipsum dolor sit amet consectetur adixpisicing elit. Temporibus laboriosam id expedita modi, ut aut vero, velit animi cum quis mollitia. Itaque, autem at. Voluptatum, voluptas magni unde, laboriosam assumenda dolores esse harum quibusdam laborum voluptate autem et fugit. Perspiciatis pariatur, voluptatem soluta perferendis repellendus consequuntur minima accusamus obcaecati eos natus labore at, libero dolorem hic! Facilis nobis harum corporis voluptatem nemo, dolores, suscipit alias dolorem, cum ipsa necessitatibus quisquam ut totam repellendus aspernatur ipsum omnis error odio! Possimus quis sunt vitae. Ea voluptates eos quae iusto ex autem, possimus voluptatum molestias nostrum doloribus ipsa animi explicabo dolor odit sint natus quam consequatur esse praesentium minus laborum eaque! Quos atque corrupti hic accusamus repudiandae repellendus natus animi incidunt. Magnam similique alias quis sunt enim placeat esse voluptates accusantium dolores. Voluptatibus quas magnam rem eos pariatur distinctio similique officia, dolor architecto dolore aut odit consequatur et amet tempore veritatis voluptatum nisi autem quibusdam fuga quod reiciendis provident vel? Totam voluptates molestias reprehenderit necessitatibus molestiae, corporis odio eaque recusandae excepturi odit provident illo, laboriosam officia eos, quos est sunt quis maiores repellendus obcaecati. Similique, unde inventore, praesentium quis officiis dolore perferendis expedita fuga quae architecto modi natus vel doloremque officia laboriosam corporis voluptatibus, molestiae rerum exercitationem est reiciendis enim et in repellendus! Cum omnis dolores, magni impedit fugit fugiat eius rem et ipsum quos porro aliquam laudantium modi inventore ratione deleniti, expedita reiciendis libero eligendi delectus dicta. Incidunt eos eveniet obcaecati quos eaque placeat ab porro distinctio itaque. Voluptates, optio eius deleniti quae impedit repellat voluptatum non ducimus vero, omnis aspernatur accusamus, mollitia illum quis doloribus reprehenderit. Accusamus praesentium, nisi in obcaecati distinctio modi iure molestiae! Rerum consectetur fugit enim eius quidem odit optio saepe iste at, earum, aperiam cumque sit doloremque architecto labore error dolores sint alias ad accusamus amet eaque tempore velit aut? Veniam impedit illo quis rem rerum ipsum vitae temporibus aut, deleniti quod, sit facilis eaque sapiente excepturi, non debitis laborum! Ea sed, quidem eos, quibusdam aliquid dolore sunt mollitia vero temporibus illo, ab voluptate corporis a! Enim, aliquid delectus laudantium suscipit voluptates veniam ullam architecto odit amet corporis maxime laboriosam nisi deleniti rem tempora. Nisi ex quia inventore minima error, voluptate, explicabo eum, sint nobis vitae cumque nihil nostrum necessitatibus corrupti molestias aperiam. Et, rerum reprehenderit? Ut maxime quae quaerat, minima magnam esse fuga, cum unde dolorum hic sapiente repellendus rem cupiditate in debitis saepe. Doloremque iusto dolorum consequatur rerum soluta et dolores ratione, voluptatibus dignissimos? Magnam quam porro odit obcaecati, aspernatur id voluptatum excepturi facere sit voluptates non inventore impedit vel voluptas molestias! Eum, iure! Amet dignissimos, in odit aliquid mollitia neque reiciendis rerum harum sunt fugiat, expedita distinctio omnis beatae corporis commodi consequuntur assumenda similique architecto cum, id voluptate deleniti fuga veritatis? Voluptatum laudantium, ea natus facilis dicta illum quod voluptate alias ipsa hic, recusandae sint id eius non placeat pariatur eaque dolorum sed. Eveniet, delectus facere. Id, dolores consequuntur aliquid cumque eius quidem nobis. Harum perferendis sed quasi officia inventore esse autem beatae dolorum?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
 
 <style scoped>
 </style>
